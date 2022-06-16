1af35dd5a14047e712176dd43a8c453183a5f65e
Ahmad Thiab <ahmad.thiab6@gmail.com>
2022-06-16 16:51:40 +0000

    Wrote a monitoing agent program that collects both hardware sepcific data and resource usage data, and then persist that data into a psql instance

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -3,11 +3,11 @@ CREATE TABLE IF NOT EXISTS PUBLIC.host_info
   (
      id               SERIAL NOT NULL PRIMARY KEY,
      hostname         VARCHAR NOT NULL UNIQUE,
-     cpu_number       int NOT NULL,
+     cpu_number       VARCHAR NOT NULL,
      cpu_architecture VARCHAR NOT NULL,
      cpu_model        VARCHAR NOT NULL,
-     cpu_mhz          FLOAT NOT NULL,
-     L2_cache         int NOT NULL,
+     cpu_mhz          VARCHAR NOT NULL,
+     L2_cache         VARCHAR NOT NULL,
      total_mem        VARCHAR NOT NULL,
      "timestamp"      TIMESTAMP NOT NULL
   );
@@ -22,12 +22,12 @@ CREATE TABLE IF NOT EXISTS PUBLIC.host_info
 CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
   (
      "timestamp"    TIMESTAMP NOT NULL,
-     host_id        SERIAL NOT NULL,
-     memory_free    int NOT NULL,
-     cpu_idle       int NOT NULL,
-     cpu_kernel     int NOT NULL,
-     disk_io        int NOT NULL,
-     disk_available int NOT NULL
+     host_id        VARCHAR NOT NULL,
+     memory_free    VARCHAR NOT NULL,
+     cpu_idle       VARCHAR NOT NULL,
+     cpu_kernel     VARCHAR NOT NULL,
+     disk_io        VARCHAR NOT NULL,
+     disk_available VARCHAR NOT NULL
   );
 
 -- DML
