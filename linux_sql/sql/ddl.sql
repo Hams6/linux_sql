b9d7b8bb549f0c441e3a76de065af1023ac71b4b
Ahmad Thiab <ahmad.thiab6@gmail.com>
2022-06-21 00:06:14 +0000

    Added an sql scripts containing 3 different queries

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -3,12 +3,12 @@ CREATE TABLE IF NOT EXISTS PUBLIC.host_info
   (
      id               SERIAL NOT NULL PRIMARY KEY,
      hostname         VARCHAR NOT NULL UNIQUE,
-     cpu_number       VARCHAR NOT NULL,
+     cpu_number       INTEGER NOT NULL,
      cpu_architecture VARCHAR NOT NULL,
      cpu_model        VARCHAR NOT NULL,
-     cpu_mhz          VARCHAR NOT NULL,
-     L2_cache         VARCHAR NOT NULL,
-     total_mem        VARCHAR NOT NULL,
+     cpu_mhz          FLOAT8 NOT NULL,
+     L2_cache         INTEGER NOT NULL,
+     total_mem        INTEGER NOT NULL,
      "timestamp"      TIMESTAMP NOT NULL
   );
 
@@ -22,12 +22,12 @@ CREATE TABLE IF NOT EXISTS PUBLIC.host_info
 CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
   (
      "timestamp"    TIMESTAMP NOT NULL,
-     host_id        VARCHAR NOT NULL,
-     memory_free    VARCHAR NOT NULL,
-     cpu_idle       VARCHAR NOT NULL,
+     host_id        INTEGER NOT NULL,
+     memory_free    INTEGER NOT NULL,
+     cpu_idle       INTEGER NOT NULL,
      cpu_kernel     VARCHAR NOT NULL,
-     disk_io        VARCHAR NOT NULL,
-     disk_available VARCHAR NOT NULL
+     disk_io        INTEGER NOT NULL,
+     disk_available INTEGER NOT NULL
   );
 
 -- DML
